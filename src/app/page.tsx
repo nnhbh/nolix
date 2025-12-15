b47cae80a2599df761b433ee0dace5561c75d842
nnhbh <menna35582@gmail.com>
2025-12-15 05:15:18 +0200

    trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import prisma from "@/lib/dp";
+
+import { caller } from "@/trpc/server";
 async function Page() {
-  const users = await prisma.user.findMany();
+  const users = await caller.getUsers();
   return (
     <div className="text-3xl text-red-400 text=center">
       {JSON.stringify(users)}
